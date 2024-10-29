344e0a0b7c9fde2e7d2fa7945875c810255507fd
Quecky <dalisbarranco@hotmail.com>
2024-10-28 20:04:21 -0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TourModule } from './tour/tour.module';
+
+@Module({
+  imports: [TourModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
