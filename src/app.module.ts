edffbb42d543d0dc22ca3cefce7e995718965844
jhonntantb <jhonntantb@gmail.com>
2024-11-02 03:17:28 -0300

    Add connection database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,32 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TourModule } from './modules/tour/tour.module';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { dataSourceOptions } from '@configuration/orm.configuration';
+import { configuration } from '@configuration/configuration';
+import { ConfigModule } from '@nestjs/config';
+import { configurationValidate } from '@configuration/configuration.validate';
 
 @Module({
-  imports: [TourModule, CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      validationSchema: configurationValidate,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...dataSourceOptions,
+        autoLoadEntities: true,
+      }),
+      dataSourceFactory: async (options) => {
+        return new DataSource(options).initialize();
+      },
+    }),
+    TourModule,
+    CategoriesModule,
+  ],
   controllers: [],
   providers: [AppService],
 })
--- src/modules/tour/tour.module.ts
+++ src/modules/tour/tour.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TourService } from './tour.service';
+import { TourService } from './application/service/tour.service';
 import { TourController } from './interface/tour.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TourEntity } from './infrastructure/persistence/entities/tour.entity';
+import { CommonModule } from '@/common/common.module';
+import { TOUR_REPOSITORY } from './application/repository/tour.repository';
+import { TourMySQLRepository } from './infrastructure/persistence/tour.myslq.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TourEntity]), CommonModule],
   controllers: [TourController],
-  providers: [TourService],
+  providers: [
+    TourService,
+    {
+      provide: TOUR_REPOSITORY,
+      useClass: TourMySQLRepository,
+    },
+  ],
 })
 export class TourModule {}
